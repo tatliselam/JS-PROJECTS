3be4fc5047e773790e4ad023d5eab32adc8be876
Abduselam Tatli <a.selamtatli@Abduselams-MacBook-Pro.local>
2020-12-28 12:09:43 -0800

    BMI compare project is  completed

--- 1st Day-Project/01-BMI-Compare - Project/script.js
+++ 1st Day-Project/01-BMI-Compare - Project/script.js
@@ -1,16 +1,35 @@
-let massMark = 50;
-let massJohn = 40;
-let heightMaqrk = 1.80;
-let heightJohn = 1.90
+//BMI:(Body Mass Index), 
+//creat variables to store mass of mark and john
+let massMark = prompt('enter marks  mass');
+let massJohn = prompt('enter John mass');
+let heightMark = prompt('enter Mark height');
+let heightJohn = prompt('enter John height');
+//2- This above lines for BMI calculate.
 let bmiJohn = massJohn / (heightJohn * heightJohn);
 let bmiMark =  massMark / (heightMark * heightMark);
-// This above lines for BMI calculate.
+//3-Print  BMIs to the console.
+console.log('Johns BMI is '+bmiJohn+' Mark BMI  is '+ bmiMark)
 
-bmiJohn = bmiJohn.toFixed(2);
-bmiJohn = bmiJohn.toFixed(2);
-
-heightBMI =  bmiJohn > bmiMark;
-
+//We have fixed to  2  decimals
+//(Math.Floor: sayiyi yuvarlatiyor.Noktadan onceki kaliyor
+// Math.Round: 4.5  sonrasini 5  yuvarlar.4.4  altindakini 4 yuvarlar
+// toFixed:3.987385687356786957826---(2 desek) --->3.99  olur.gibi
+//4-shorten BMI results
+bmiJohn = Math.floor(bmiJohn);
+bmiJohn = Math.floor(bmiMark);
+//5-create a boolean variable to  assign higherBMI  
+let higherBMI =  bmiJohn > bmiMark;
 //const higherBMI = A > B ;//this will gives "true"
 //const higherBMI = A < B ;//this will gives "false"
-console.log (`Is Mark's BMI is higher than John's? Answer is ${higherBMI}. Because Mark's BMI is ${bmiMark} and John's BMI is ${bmiJohm}.`)
+
+//6-Use console.log and string interpolation to   log the result
+console.log (`Is Mark's BMI is higher than John's? Answer is ${higherBMI}. Because Mark's BMI is ${bmiMark} and John's BMI is ${bmiJohn}.`)
+
+//Additional step
+if(bmiJohn > bmiMark){
+  console.log('John BMI is greater than Mark')
+}else if(bmiJohn < bmiMark){
+  console.log('John BMI is less than Mark')
+}else {
+  console.log('John BMI equals to Mark')
+}
